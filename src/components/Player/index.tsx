d46c26f8a9faf78aa82eba2c30122ace5ae9163e
luizmrri <74222433+luizmrri@users.noreply.github.com>
2021-04-21 00:15:40 -0300

    gh

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,11 +1,43 @@
 import styles from './styles.module.scss'
 
 export default function Player() {
-    
-
     return(
-        <div>
-            
+        <div className={styles.PlayerContainer}>
+            <header>
+                <img src="/playing.svg" alt="Tocando agora"/>
+                <strong>Tocando Agora</strong>
+            </header>
+
+            <div className={styles.emptyPlayer}>
+                <strong>Selecione um Podcast para ouvir</strong>
+            </div>
+
+            <footer className={styles.empty}>
+                <div className={styles.progress}>
+                    <span>00:00</span>
+                    <div className={styles.Slider}/>
+                    <div className={styles.emptySlider}/>
+                    <span>00:00</span>
+                </div>
+
+                <div className={styles.buttons}>
+                    <button type="button">
+                        <img src="/shuffle.svg" alt="Embaralhar"/>
+                    </button>
+                    <button type="button">
+                        <img src="/play-previous.svg" alt="Tocar anterior"/>
+                    </button>
+                    <button type="button" className={styles.playButton}>
+                        <img src="/play.svg" alt="Tocar"/>
+                    </button>
+                    <button type="button" >
+                        <img src="/play-next.svg" alt="Tocar próxima"/>
+                    </button>
+                    <button type="button" >
+                        <img src="/repeat.svg" alt="Repetir"/>
+                    </button>
+                </div>
+            </footer>
         </div>
     )
 }
