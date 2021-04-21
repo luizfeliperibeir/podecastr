242f90e39a87006cfd294d7b9de85c071a116504
luizmrri <74222433+luizmrri@users.noreply.github.com>
2021-04-20 23:25:13 -0300

    hh

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,18 @@
-import styles from ' ./styles.module.scss'
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import styles from './styles.module.scss'
 
 export default function Header() {
+    const currentDate = format (new Date(), 'EEEEEE, d, MMMM',{
+        locale: ptBR
+    })
+
     return(
         <header className={styles.HeaderContainer}>
             <img src="/logo.svg" alt="logo"/>
-            <p>O melhor para você ouvir, sempre</p>
-            <span>Qui, 8 de Abril</span>
+            <p >O melhor para você ouvir, sempre</p>
+            <span >{currentDate}</span>
         </header>
     )
 }
