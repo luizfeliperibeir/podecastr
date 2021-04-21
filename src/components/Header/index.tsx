a4cde367b9372dcd3d126cda677d46ba7a8d448a
luizmrri <74222433+luizmrri@users.noreply.github.com>
2021-04-20 22:47:48 -0300

    er

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Header() {
+    return(
+        <header className="HeaderContainer">
+            <img src="/logo.svg" alt="logo"/>
+            <p>O melhor para você ouvir, sempre</p>
+            <span>Qui, 8 de Abril</span>
+        </header>
+    )
+}
