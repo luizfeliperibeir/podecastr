72350f1f51a457fa186ca244603597b5c1668f07
luizmrri <74222433+luizmrri@users.noreply.github.com>
2021-04-20 22:53:53 -0300

    f

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,8 @@
-
+import styles from ' ./styles.module.scss'
 
 export default function Header() {
     return(
-        <header className="HeaderContainer">
+        <header className={styles.HeaderContainer}>
             <img src="/logo.svg" alt="logo"/>
             <p>O melhor para você ouvir, sempre</p>
             <span>Qui, 8 de Abril</span>
